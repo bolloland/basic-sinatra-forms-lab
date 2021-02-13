7737c95bd10b2d999ec2142f6b4666fd5cdb31b4
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-02-13 13:15:36 -0600

    commit tree 645eb57bbb17b33f886bc64219a26e1a68468b61

--- app.rb
+++ app.rb
@@ -2,5 +2,22 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get "/newteam" do
+        erb :newteam
+    end
+
+    post "/team" do
+       
+        puts params
+        @team_name = params[:name]
+        @coach = params[:coach]
+        @point = params[:pg]
+        @shoot = params[:sg]
+        @small = params[:sf]
+        @power = params[:pf]
+        @cent = params[:c]
+        
+        erb :team
+    end
 
 end
